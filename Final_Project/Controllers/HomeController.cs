e8cae2f55ffd201dee04efc1811762405323fde9
jkindle26 <jkindle@gmail.com>
2020-01-08 10:48:35 -0600

    same as previous

--- Final_Project/Controllers/HomeController.cs
+++ Final_Project/Controllers/HomeController.cs
@@ -1,29 +1,93 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using Final_Project.Models;
 
-namespace IdentitySample.Controllers
+namespace Final_Project.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
-        [HttpGet]
-        [Authorize]
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
+
 
             return View();
         }
 
-        [HttpGet]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
 
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModels cvm)
+        {
+            if (ModelState.IsValid)
+            {
+                string message = $"You have recieved a email from {cvm.Name} " +
+                    $"With the subject {(string.IsNullOrEmpty(cvm.Subject) ? "No Subject" : cvm.Subject)} " +
+                    $"Please respond to {cvm.Email} <br/>Message:<br/>" +
+                    $"<em>{cvm.Message}</em>";
+
+                MailMessage mm = new MailMessage(
+                    "jkindle26@gmail.com",
+                    "jkindle26@outlook.com",
+                    string.IsNullOrEmpty(cvm.Subject) ? "No Subject" : cvm.Subject,
+                    message
+                    );
+
+                mm.IsBodyHtml = true;
+                mm.Priority = MailPriority.High;
+                mm.ReplyToList.Add(cvm.Email);
+
+                SmtpClient client = new SmtpClient("mail.kindledev.com");
+                client.Port = 8889;
+                client.Credentials = new NetworkCredential(
+                    "jkindle@kindledev.com", "P@ssw0rd");
+
+                try
+                {
+                    client.Send(mm);
+                }
+                catch (Exception e)
+                {
+
+                    ViewBag.FailedEmailMessage =
+                        $"We're sorry your request was not completed at this time." +
+                        $" Please try again later. Error Message: <br/>{e.StackTrace}";
+                    return View(cvm);
+                }
+
+
+                return View("Index");
+
+
+
+            }//ModelStatIsValid
+            return View(cvm);
+
+        }
+
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult Team()
+        {
             return View();
         }
     }
--- Final_Project/Models/ContactViewModel.cs
+++ Final_Project/Models/ContactViewModel.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Final_Project.Models
 {
-    public class ContactViewModel
+    public class ContactViewModels
     {
+        [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "***Name is async required field***")]
+        public string Name { get; set; }
+
+        [StringLength(25, ErrorMessage = "*** Email too long ***")]
+        [Required(ErrorMessage = "***Email is async required field***")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "*** Please enter a valid email ***")]
+        //in JavaScript we validated our emaili testing it against a
+        //regulat expression, in C# (server-side validation) we have this
+        //built-in to a data annotation
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "*** Message is a required field ***")]
+        //we do not want our message area in the form to be a text box
+        //we want this to be a textarea not input type="text"
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
     }
 }
